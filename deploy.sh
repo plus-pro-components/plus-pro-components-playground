615d967c37734d5aacbd0d765c57ad669f53b1c6
xiaofei <lixiaofei0627@gmail.com>
2025-05-15 14:03:27 +0800

    edit sh

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,6 @@ git commit -m 'deploy'
 
 git push -f https://github.com/plus-pro-components/plus-pro-components-playground.git master:gh-pages
 
+rsync -av --exclude=".*" ./ root@47.109.84.50:/root/plus-pro-components-playground
+
 cd -
